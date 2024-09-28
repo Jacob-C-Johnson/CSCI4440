564646e0212a495e6a98b730e86cd434f74e3b39
Jacob Johnson <jacobj1789@gmail.com>
2024-09-28 13:53:56 -0400

    Rotation with custom functions

--- Changed/Practice2d.js
+++ Changed/Practice2d.js
@@ -4,18 +4,18 @@ var canvas;
 var gl;
 
 var pointsArray = [];
-var colors = [];
 
 var xAxis = 0;
 var yAxis = 1;
 var zAxis = 2;
 
 var axis = 0;
-var theta = [0, 0, 0];
+var theta = [0, 0, 0.1];
 
-var toggle = [1, 1, 1];
+var center = vec3(0.4, 0.4, 0); // Center of the square
 
 var thetaLoc;
+var mLoc;
 
 window.onload = function init()
 {
@@ -29,35 +29,45 @@ window.onload = function init()
 
     gl.enable(gl.DEPTH_TEST);
 
-    //
-    //  Load shaders and initialize attribute buffers
-    //
-
+    // Load shaders and initialize attribute buffers
     var program = initShaders(gl, "vertex-shader", "fragment-shader");
     gl.useProgram(program);
 
-    var positionLoc = gl.getAttribLocation(program, "aPosition");
-    gl.vertexAttribPointer(positionLoc, 4, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(positionLoc);
-
-    thetaLoc = gl.getUniformLocation(program, "uTheta");
-
-    pointsArray.push(vec4( 0.2, 0.2, 0, 1));
-    pointsArray.push(vec4( 0.6, 0.2, 0, 1));
-    pointsArray.push(vec4( 0.6, 0.6, 0, 1));
-    pointsArray.push(vec4( 0.2, 0.6, 0, 1));
+    pointsArray.push(vec4(0.2, 0.2, 0, 1));
+    pointsArray.push(vec4(0.6, 0.2, 0, 1));
+    pointsArray.push(vec4(0.6, 0.6, 0, 1));
+    pointsArray.push(vec4(0.2, 0.6, 0, 1));
 
     var vBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW);
 
+    var positionLoc = gl.getAttribLocation(program, "aPosition");
+    gl.vertexAttribPointer(positionLoc, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(positionLoc);
+
+    mLoc = gl.getUniformLocation(program, "uModelViewMatrix");
+
     render();
 }
 
-
 function render()
 {
-    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    // Rotate the square
+    theta[zAxis] += 0.1;
+
+    var ModelViewMatrix = mat4();
+    ModelViewMatrix = mult(ModelViewMatrix, translate(0.4,0.4,0));
+
+    var r = rotateZ(theta[zAxis]);
+    ModelViewMatrix = mult(ModelViewMatrix, r);
+
+    ModelViewMatrix = mult(ModelViewMatrix, translate(-0.4,-0.4,0));
+
+    gl.uniformMatrix4fv(mLoc, false, flatten(ModelViewMatrix));
+
 
     gl.drawArrays(gl.TRIANGLE_FAN, 0, pointsArray.length);
     requestAnimationFrame(render);
